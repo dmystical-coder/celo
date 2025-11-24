c02ba3e889d810b93afae86d8810e52ffd91147f
dmystical-coder <dmystical.coder@gmail.com>
2025-11-24 01:05:40 +0100

    feat: add header and instructions to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import './styles/wordle.css';
 import { GameProvider } from './components/GameProvider';
 import { Game } from './components/Game';
+import { Header } from './components/Header';
 
 export default function Home() {
   return (
-    <GameProvider>
-      <Game />
-    </GameProvider>
+    <>
+      <Header />
+      <GameProvider>
+        <Game />
+      </GameProvider>
+    </>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Header: React.FC = () => (
+  <header className="header">
+    <h1>Wordle</h1>
+    <p>Guess the 5-letter word in 6 tries!</p>
+  </header>
+);
